8672ce504b4bfeb9fcfd5916ae96f8e46882e26d
victorjaquez <victor@jaquez.me>
2019-03-15 09:04:03 -0700

    table component, installed dependencies, set up module import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Ng2SmartTableModule } from 'ng2-smart-table';
 import { AppComponent } from './app.component';
+import { TableComponent } from './table/table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    Ng2SmartTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
